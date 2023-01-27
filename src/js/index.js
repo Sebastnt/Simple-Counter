bf09753b97b24172712241013f1d49ed3a31829e
Sebastnt <sebastnt.p@hotmail.com>
2023-01-27 02:11:36 +0000

    Updated project functional

--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,31 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+
+let counter = 0;
+
+function multipleTask() {
+	function tick() {
+		counter ++;
+		let counterarray = counter.toString().padStart(6,0).split('');
+		
+		const element = ( 
+			<div id='main-box'>
+				<div className="box fs-1"><i class="far fa-clock"></i></div>
+				{
+				counterarray.map((count,i) => (
+				<div className="box">
+					<h2 key={i}>{count}</h2>
+				</div>))
+				}
+			</div>
+		)
+		ReactDOM.render(element, document.querySelector("#app"));
+	}
+	setInterval(tick, 1000);
+}
+
+multipleTask()
+
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+// ReactDOM.render(<Home />, document.querySelector("#app"));
